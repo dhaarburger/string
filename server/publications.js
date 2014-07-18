77e3b41e68cb2bf7735612b3a5b7f963f67f6d49
Daniel Haarburger <work.haarburger@gmail.com>
2014-07-18 00:35:45 -0700

    turning in for the night. cleans up page subscription

--- server/publications.js
+++ server/publications.js
@@ -2,6 +2,6 @@ Meteor.publish('strings', function(){
 	return Strings.find();
 });
 
-Meteor.publish('pages', function(){
-	return Pages.find();
+Meteor.publish('pages', function(stringId){
+	return Pages.find({stringId: stringId});
 });
